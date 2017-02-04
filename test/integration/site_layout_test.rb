b18480709f69ff25b35f8fa86dc55f55c4a67b43
Frank Torchia <ftt9282@rit.edu>
2017-02-03 22:21:12 -0500

    re-commiting

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +13,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test 'layout login links' do
+  	log_in_as @user
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 end
